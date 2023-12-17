4252f65f27d20de37926b489ca52f7b6a649d193
Luan Victor <lvictor.dutra@gmail.com>
2023-12-16 21:42:38 -0300

    refact: improve code and add features

--- src/modules/setupManager/repositories/implementations/SetupManagerRepositoryImplementations.ts
+++ src/modules/setupManager/repositories/implementations/SetupManagerRepositoryImplementations.ts
@@ -14,96 +14,98 @@ export class SetupManagerRepositoryImplementation
     private depedenciesInstallerRepository: DepedenciesInstallerRepository,
   ) {}
 
-  async installDependencies(
-    answers: Pick<Answers, "wichLanguage" | "willLint" | "wichManager">,
-  ): Promise<void> {
-    const { wichLanguage, willLint, wichManager } = answers;
-    const managerInstallCommand: string = managers[wichManager].installCommand;
+  async installDependencies({
+    wichLanguage,
+    willLint,
+    wichManager,
+  }: Pick<
+    Answers,
+    "wichLanguage" | "willLint" | "wichManager"
+  >): Promise<void> {
+    const { installCommand } = managers[wichManager];
     const isTypescript = wichLanguage === "Typescript";
 
     if (isTypescript) {
       await this.depedenciesInstallerRepository.install(
-        managerInstallCommand,
+        installCommand,
         wichLanguage,
       );
     }
 
     if (willLint === "Yes") {
-      if (isTypescript) {
-        await this.depedenciesInstallerRepository.install(
-          managerInstallCommand,
-          "EslintTS",
-        );
-      }
-
       await this.depedenciesInstallerRepository.install(
-        managerInstallCommand,
-        "Eslint",
+        installCommand,
+        isTypescript ? "EslintTS" : "Eslint",
       );
     }
   }
 
-  async setupConfigurations(
-    answers: Pick<Answers, "hasPackageJson" | "wichManager" | "isVscode">,
-  ): Promise<void> {
-    const { hasPackageJson, isVscode, wichManager } = answers;
+  async setupConfigurations({
+    hasPackageJson,
+    isVscode,
+    wichManager,
+    wichLanguage,
+    willLint,
+  }: Pick<
+    Answers,
+    "hasPackageJson" | "isVscode" | "wichManager" | "wichLanguage" | "willLint"
+  >): Promise<void> {
+    const isTypescript = wichLanguage === "Typescript";
+    const currentPath = new URL(".", import.meta.url).pathname;
+    const rootPath = path.resolve(currentPath, "../../../../..");
+    const templatesPath = (subpath: string) =>
+      path.join(
+        rootPath,
+        "src",
+        "modules",
+        "setupManager",
+        "templates",
+        subpath,
+      );
+
+    const copyFiles = async (source: string, destination: string) => {
+      try {
+        fs.mkdirSync(path.dirname(destination), { recursive: true });
+        fs.copyFileSync(source, destination);
+      } catch (error) {
+        console.error(`Erro ao copiar ${source} para ${destination}:`, error);
+      }
+    };
 
     if (isVscode === "Yes") {
-      try {
-        const currentPath = new URL(".", import.meta.url).pathname;
-
-        const rootPath = path.resolve(currentPath, "../../../../..");
-
-        const settingsJson = path.join(
-          rootPath,
-          "src",
-          "modules",
-          "setupManager",
-          "templates",
-          "ide",
-          "vscode",
-          "settings.json",
-        );
-
-        const editorconfig = path.join(
-          rootPath,
-          "src",
-          "modules",
-          "setupManager",
-          "templates",
-          "ide",
-          "vscode",
-          ".editorconfig",
-        );
-
-        const settingsContent = fs.readFileSync(settingsJson, "utf-8");
-        // const editorconfigContent = fs.readFileSync(editorconfig, "utf-8");
-
-        const settingsObject = JSON.parse(settingsContent);
-
-        fs.mkdirSync("./mock/.vscode", { recursive: true });
-
-        fs.writeFileSync(
-          "./mock/.vscode/settings.json",
-          JSON.stringify(settingsObject, null, 2),
-          "utf-8",
-        );
-        fs.copyFileSync(editorconfig, "./mock/.editorconfig");
-
-        console.log("Configurações do VSCode atualizadas com sucesso!");
-      } catch (error) {
-        console.error("Erro ao configurar o VSCode:", error);
-      }
+      await copyFiles(
+        templatesPath("ide/vscode/.editorconfig"),
+        "./mock/.editorconfig",
+      );
+      await copyFiles(
+        templatesPath("ide/vscode/settings.json"),
+        "./mock/.vscode/settings.json",
+      );
     }
 
-    try {
-      fs.mkdirSync("./mock/src", { recursive: true });
-    } catch (error) {
-      console.error("Erro ao configurar o src:", error);
+    if (isTypescript) {
+      await copyFiles(
+        templatesPath("typescript/tsconfig.json"),
+        "./mock/tsconfig.json",
+      );
     }
+
+    if (willLint === "Yes") {
+      isTypescript
+        ? await copyFiles(
+            templatesPath("lint/typescript/.eslintrc.json"),
+            "./mock/.eslintrc.json",
+          )
+        : await copyFiles(
+            templatesPath("lint/javascript/.eslintrc.json"),
+            "./mock/.eslintrc.json",
+          );
+    }
+
+    fs.mkdirSync("./mock/src", { recursive: true });
 
     if (hasPackageJson === "No") {
-      const initCommand: string = managers[wichManager].initCommand;
+      const { initCommand } = managers[wichManager];
       await this.initializeNewProjectRepository.install(initCommand);
     }
   }
