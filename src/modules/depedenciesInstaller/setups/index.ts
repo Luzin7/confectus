b0031798b6998598f7486cec067c11059534ed94
Luan Victor <lvictor.dutra@gmail.com>
2023-12-16 21:41:02 -0300

    update: add configfiles configuration to dependencies setup

--- src/modules/depedenciesInstaller/setups/index.ts
+++ src/modules/depedenciesInstaller/setups/index.ts
@@ -2,17 +2,35 @@ import { SettingsProps } from "src/types/setting";
 
 export const dependeciesSetup: SettingsProps = {
   typescript: {
+    configFiles: [
+      {
+        configFileName: "tsconfig.json",
+        configFilePath: "typescript/tsconfig.json",
+      },
+    ],
     dependencies: "tsx",
     devDependencies: "typescript @types/node ts-node tsup",
   },
   eslint: {
+    configFiles: [
+      {
+        configFileName: ".eslint.json",
+        configFilePath: "lint/javascript/.eslint.json",
+      },
+    ],
     dependencies: null,
     devDependencies:
       "eslint prettier eslint-plugin-prettier eslint-config-prettier",
   },
   eslintts: {
+    configFiles: [
+      {
+        configFileName: ".eslint.json",
+        configFilePath: "lint/typescript/.eslint.json",
+      },
+    ],
     dependencies: null,
     devDependencies:
-      "@typescript-eslint/eslint-plugin@latest @typescript-eslint/parser@latest",
+      "eslint prettier @typescript-eslint/eslint-plugin @typescript-eslint/parser eslint-config-prettier eslint-plugin-prettier",
   },
 };
