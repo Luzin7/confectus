f574c4f5196a9c237c86643bb46b2bfc449ee938
Luan Victor <lvictor.dutra@gmail.com>
2023-12-16 21:44:19 -0300

    feat: add loading and error message

--- src/modules/setupManager/useCases/SetupManager.ts
+++ src/modules/setupManager/useCases/SetupManager.ts
@@ -1,11 +1,25 @@
 import { UseCase } from "@shared/core/modules/UseCase";
 import { SetupManagerRepository } from "../repositories/contracts/SetupManagerRepository";
+import chalk from "chalk";
+import { createSpinner } from "nanospinner";
 
 export class SetupManager implements UseCase<Record<string, string>> {
   constructor(private setupManagerRepository: SetupManagerRepository) {}
 
   async execute(answers: Record<string, string>): Promise<void> {
-    await this.setupManagerRepository.setupConfigurations(answers);
-    await this.setupManagerRepository.installDependencies(answers);
+    const spinner = createSpinner(
+      `We're rushing to setup your project!`,
+    ).start();
+    try {
+      await this.setupManagerRepository.setupConfigurations(answers);
+      await this.setupManagerRepository.installDependencies(answers);
+      spinner.success({ text: "Project setup completed successfully!" });
+    } catch (error) {
+      spinner.error({
+        text: chalk.red(`The process has failed...
+      \n ${error}`),
+      });
+      process.exit(1);
+    }
   }
 }
