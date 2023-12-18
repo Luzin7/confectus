084fdb19d5af3c26bc3faa7fd3c02f6ffb6efa5b
Luan Victor <lvictor.dutra@gmail.com>
2023-12-18 19:31:35 -0300

    feat: configure tsup

--- tsup.config.js
+++ tsup.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from "tsup";
-
+import rollup from "rollup-plugin-multi-entry";
 export default defineConfig({
   entry: ["src/**/*.ts"],
   format: "esm",
   outDir: "./dist",
+  plugins: [rollup()],
+  loader: { ".*": "copy" },
 });
