694ad4c07b512bb5844f107bbf8b886790d3d655
Luzin7 <lvictor.dutra@gmail.com>
2024-03-23 15:32:28 -0300

    feat: make questionnaire more dynamic

--- src/modules/questionnaire/repositories/implementations/QuestionnaireRepositoryImplementations.ts
+++ src/modules/questionnaire/repositories/implementations/QuestionnaireRepositoryImplementations.ts
@@ -1,12 +1,21 @@
-import { QuestionnaireRepository } from "../contracts/QuestionnaireRepository";
+import {
+  backendQuestions,
+  frontendQuestions,
+  wichStackQuestion,
+} from "@/infra/cli/questions";
 import inquirer from "inquirer";
 import { QuestionnaireError } from "../../errors/QuestionnaireError";
-import questions from "@/infra/cli/questions";
+import { QuestionnaireRepository } from "../contracts/QuestionnaireRepository";
 
 export class QuestionnaireRepositoryImplementations
   implements QuestionnaireRepository
 {
+  private stackChoice: string = "";
   private answers: Record<string, string> = {};
+
+  private wichStackWillUse(stackChoice: Record<string, string>) {
+    this.stackChoice = stackChoice.wichStack.toLowerCase();
+  }
 
   private saveAnswers(answers: Record<string, string>) {
     this.answers = { ...this.answers, ...answers };
@@ -14,8 +23,16 @@ export class QuestionnaireRepositoryImplementations
 
   async startQuestionnaire(): Promise<void> {
     try {
-      const responses = await inquirer.prompt(questions);
-      this.saveAnswers(responses);
+      const stack: Record<string, string> =
+        await inquirer.prompt(wichStackQuestion);
+      this.wichStackWillUse(stack);
+
+      const isFrontend: boolean = this.stackChoice === "frontend";
+
+      const questions = isFrontend ? frontendQuestions : backendQuestions;
+      const answers: Record<string, string> = await inquirer.prompt(questions);
+
+      return this.saveAnswers(answers);
     } catch (error) {
       return console.error(new QuestionnaireError());
     }
