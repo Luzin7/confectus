7d9657b81cc30e05a223ea4bc774d426b0e6edb0
Luzin7 <lvictor.dutra@gmail.com>
2024-06-30 22:13:30 -0300

    refactor: update old eslint config to new eslint config

--- src/templates/backend/linters/eslint/typescript/eslint.config.mjs
+++ src/templates/backend/linters/eslint/typescript/eslint.config.mjs
@@ -1,19 +1,59 @@
-import globals from 'globals';
-
 import { FlatCompat } from '@eslint/eslintrc';
-import pluginJs from '@eslint/js';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import js from '@eslint/js';
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import globals from 'globals';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 
-// mimic CommonJS variables -- not needed if using CommonJS
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-  recommendedConfig: pluginJs.configs.recommended,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
 export default [
-  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
-  ...compat.extends('standard-with-typescript'),
+  ...compat.extends(
+    'standard',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ),
+  {
+    plugins: {
+      '@typescript-eslint': typescriptEslint,
+    },
+
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+
+      parser: tsParser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+
+    settings: {
+      'import/parsers': {
+        '@typescript-eslint/parser': ['.ts', '.tsx', '.d.ts'],
+      },
+    },
+
+    rules: {
+      'no-useless-constructor': 'off',
+      'prettier/prettier': [
+        'error',
+        {
+          printWidth: 80,
+          tabWidth: 2,
+          singleQuote: true,
+          trailingComma: 'all',
+          arrowParens: 'always',
+          semi: true,
+        },
+      ],
+    },
+  },
 ];
